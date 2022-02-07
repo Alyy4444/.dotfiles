f8fcf5f58975c69ce40d3fec41235c358d2a4fc2
Aly <98731150+Alyy4444@users.noreply.github.com>
2022-02-07 17:11:27 +0800

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,13 +6,10 @@ module.exports = {
     devOnly: false,
     run: async ({client, message, args}) => {
         const help = new Discord.MessageEmbed()
-        .setTitle("Help Guide for Reminder Bot")
-		.addField('!remind', 'type \'!remind (time) (your reminder)\' for reminders. Here\'s some examples \'!remind 1d1h10m30s eat your veggies!\', \'!remind 23h10s go to sleep\'')
+        .setTitle("Commands for the bot")
 		.addField('!hello', 'says hello to the bot')
 		.addField('!say', 'will repeat what you say')
 		.addField('!reminders', 'lists out your reminders')
-		.addField('!info', 'says a info about the bot')
-		.addField('!G5', 'says the member of the Group')
 		.setColor('#40bcaf')
 
 		message.channel.send({ embeds: [help] })
