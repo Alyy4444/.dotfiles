f17f29d60cba84cd61439875e608777df423ef45
Aly <98731150+Alyy4444@users.noreply.github.com>
2022-02-07 16:40:26 +0800

    Changes

--- commands/info/Pog.js
+++ commands/info/Pog.js
@@ -4,6 +4,6 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        message.reply("POGGERS")
+        message.reply("Poggers")
     }
 }
--- commands/info/say.js
+++ commands/info/say.js
@@ -6,7 +6,7 @@ module.exports = {
     run: async ({client, message, args}) => {
 
             /* This takes the sentence sent, and makes it an array. In this case, a list of words. It 'splits' the list up wherever it sees space.*/
-            let sentence = msg.content.split(" ");
+            let sentence = message.content.split(" ");
             
             /* .shift(), alters the list. It removes the first thing in the list. This would be the "!say" word. If we assigned shift() to a variable. Like "let x = msg.shift()" ... "x" would be the word that was removed. This is handy for grabbing command words. If you used shift() again, it would remove the second, then the third, each time that you type it.*/
             sentence.shift();
